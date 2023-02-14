7c3644cccd8c436021e5fe652dee5cdb1210539c
Hansunbae <hbj6398@naver.com>
2023-02-14 11:49:53 +0900

    panel 연습

--- MySwing/src/ch01/NoLayoutEx2.java
+++ MySwing/src/ch01/NoLayoutEx2.java
@@ -4,13 +4,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class NoLayoutEx2 extends JFrame {
-	private JButton button1;
-	private JButton button2;
-	private JButton button3;
-	private JButton button4;
-	private JButton button5;
-	private JButton button6;
-	private JButton button7;
+	private JButton[] buttons = new JButton[10];
 	
 	public NoLayoutEx2() {
 		initData();
@@ -21,28 +15,44 @@ public class NoLayoutEx2 extends JFrame {
 		setTitle("한 번 해보는 실습");
 		setSize(1000, 1000);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i]=new JButton("button"+(i+1));
+		}
 		
-		button1 = new JButton("button1");
-		button1.setSize(150, 150);
-		button2 = new JButton("button2");
-		button2.setSize(50, 50);
-		button3 = new JButton("button3");
-		button3.setSize(50, 50);
-		button4 = new JButton("button4");
-		button4.setSize(150, 150);
+		if(buttons!=null) {
+			
+			buttons[0].setSize(150, 150);
+			buttons[1].setSize(50, 50);
+			buttons[2].setSize(50, 50);
+			buttons[3].setSize(250, 150); // 몸통
+			buttons[4].setSize(50,150); 
+			buttons[5].setSize(50,150);
+			buttons[6].setSize(60,30);
+			buttons[7].setSize(60,30);
+			buttons[8].setSize(60,30);
+			buttons[9].setSize(60,30);
+		}
 		
 	}
 
 	private void setInitLayout() {
 		setLayout(null);
-		add(button1);
-		button1.setLocation(400,400);
-		add(button2);
-		button2.setLocation(350,500);
-		add(button3);
-		button3.setLocation(550,500);
-		setVisible(true);
-		button4.setLocation(600,600);
+		for (int i = 0; i < buttons.length; i++) {
+			add(buttons[i]);
+		}
+		
+		buttons[0].setLocation(400,400);
+		buttons[1].setLocation(350,500);
+		buttons[2].setLocation(550,500);
+		buttons[3].setLocation(350,550);
+		buttons[4].setLocation(400,700);
+		buttons[5].setLocation(500,700);
+		buttons[6].setLocation(395, 850);
+		buttons[7].setLocation(495, 850);
+		buttons[8].setLocation(495, 850);
+		buttons[9].setLocation(495, 850);
+		
+		
 		setVisible(true);
 	}
 	public static void main(String[] args) {
